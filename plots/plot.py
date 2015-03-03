f43d06aed33042dff1a07734228faf64dce5d65c
Jim <jim.holmstroem@gmail.com>
2015-03-03 21:02:33 +0100

    First revision of plots

--- plots/plot.py
+++ plots/plot.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python
 from __future__ import division, print_function
 
+import os
 
-def main(filename):
-    print(filename)
+import pandas
+
+
+data_location = '../data/'
+plot_location = '.'
+
+
+def read_csv(plotname):
+    return pandas.read_csv(
+        os.path.join(data_location, '{}.csv'.format(plotname)),
+        index_col=0,
+    )
+
+
+def main(plotname):
+    ax = read_csv(plotname).plot()
+
+    ax.get_figure().savefig(
+        os.path.join(
+            plot_location,
+            '{}.pdf'.format(plotname),
+        )
+    )
 
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Render PDF of the csv data')
-    parser.add_argument('filename', metavar='filename', type=str)
+    parser.add_argument('plotname', metavar='plotname', type=str)
 
     args = parser.parse_args()
 
     main(
-        filename=args.filename,
+        plotname=args.plotname,
     )
